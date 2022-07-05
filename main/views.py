1e270d7c04dc35344fcaa74aa2b2a4b10afbd6cb
anand <anand00525@gmail.com>
2022-07-05 12:09:53 +0530

    first_set_change

--- main/views.py
+++ main/views.py
@@ -144,7 +144,7 @@ def replyPage(request):
 def searchResults(request):
     search_question = request.GET.get('search')
     if search_question:
-        questions = Question.objects.filter(Q(title__icontains=search_question) | Q(body__icontains=search_question)|Q(tags_name__icontains=search_question))[:10]
+        questions = Question.objects.filter(Q(title__icontains=search_question) | Q(body__icontains=search_question)|Q(tags__name__icontains=search_question))[:10]
     else:
     # If not searched, return default posts
         questions = Question.objects.all().order_by('-created_at')[:10]   
